810f0a3436bb773bc0d3bcbd20a7432ccf757275
Miretz <miretz@gmail.com>
2016-10-02 14:40:16 +0200

    Automatically create Aiming Component on Tank with Bleprint Callable reference to the Barrel

--- BattleTank/Source/BattleTank/Public/Tank.h
+++ BattleTank/Source/BattleTank/Public/Tank.h
@@ -3,6 +3,7 @@
 #pragma once
 
 #include "GameFramework/Pawn.h"
+#include "TankAimingComponent.h"
 #include "Tank.generated.h"
 
 UCLASS()
@@ -11,7 +12,16 @@ class BATTLETANK_API ATank : public APawn
 	GENERATED_BODY()
 
 public:
+	
 	void AimAt(FVector HitLocation);
+
+	UFUNCTION(BlueprintCallable, Category = Setup)
+	void SetBarrelReference(UStaticMeshComponent* BarrelToSet);
+
+protected:
+	
+	UTankAimingComponent* TankAimingComponent = nullptr;
+	
 
 private:
 	// Sets default values for this pawn's properties
