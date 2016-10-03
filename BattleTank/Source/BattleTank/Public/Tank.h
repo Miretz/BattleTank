4b325466468c3451ca5b85598208918eace9ceb1
Miretz <miretz@gmail.com>
2016-10-03 22:18:41 +0200

    Fixed aiming problem, forward declarations

--- BattleTank/Source/BattleTank/Public/Tank.h
+++ BattleTank/Source/BattleTank/Public/Tank.h
@@ -3,10 +3,11 @@
 #pragma once
 
 #include "GameFramework/Pawn.h"
-#include "TankAimingComponent.h"
 #include "Tank.generated.h"
 
-class UTankBarrel; // Forward Declaration
+// Forward Declarations
+class UTankBarrel; 
+class UTankAimingComponent;
 
 UCLASS()
 class BATTLETANK_API ATank : public APawn
